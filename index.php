3e4e112a8deb7f32766bc7abfadeb99cd707015e
Canterla11 <acanterlagonzalez@gmail.com>
2024-10-08 13:46:00 +0200

    index.php editado
    editado el php para mostrar numero random

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$randomNumber = rand(1, 100);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Número Aleatorio</title>
+</head>
+<body>
+    <h1>Número Aleatorio</h1>
+    <p>Número aleatorio: <?php echo $randomNumber; ?></p>
+</body>
+</html>
